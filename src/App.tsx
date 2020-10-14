2020f61e497c1cd7c1a42531cbe603591252808f
Tao Hansen <taohansens@gmail.com>
2020-10-14 18:16:53 -0300

    Add logo in home, add images and styles to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+      </div>
     </div>
   );
 }
