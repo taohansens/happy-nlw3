50a7356b5480a692ac0241fe20264814e6c748f8
Tao Hansen <taohansens@gmail.com>
2020-10-14 20:35:55 -0300

    Add orpahanages page, reanme files

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import '../styles/pages/orphanages-map.css'
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+
 
 function OrphanagesMap(){
     return(
-        <h1>Testing...</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>São Raimundo Nonato</strong>
+                    <span>Piauí</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     )
 }
 
