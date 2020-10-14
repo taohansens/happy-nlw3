58e84e5fe5bf8b822fde5e9c9a9ce6fe88531b28
Tao Hansen <taohansens@gmail.com>
2020-10-14 18:34:35 -0300

    landing page ok

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,20 @@ function App() {
     <div id="page-landing">
       <div className="content-wrapper">
         <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>São Raimundo Nonato</strong>
+          <span>Piauí</span>
+        </div>
+
+        <a href="" className="enter-app">
+
+        </a>
       </div>
     </div>
   );
