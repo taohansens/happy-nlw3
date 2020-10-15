9f536995511338c10384e0265884e81389a00e4a
Tao Hansen <taohansens@gmail.com>
2020-10-15 07:11:21 -0300

    Method post ok, create in BD

--- backend-node/src/server.ts
+++ backend-node/src/server.ts
@@ -1,14 +1,39 @@
 import express, { response } from 'express';
+import { getRepository } from 'typeorm';
 
 import './database/connection';
+import Orphanage from './models/Orphanage';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/orphanages', (request,response) => {
-    console.log(request.body);
-    return response.json({ message: 'Hello World' })
+app.post('/orphanages', async(request,response) => {
+    const{
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+
+    await orphanagesRepository.save(orphanage);
+    
+    return response.status(201).json(orphanage);
 });
 
 app.listen(3333);
