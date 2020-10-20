0ca7c7490780528e2ac8383deadff9f9d9266a22
Tao Hansen <taohansens@gmail.com>
2020-10-19 22:33:56 -0300

    Header ok, CreatOrphanage

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -2,19 +2,35 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 interface HeaderProps {
     title: string;
+    showCancel?: boolean;
 }
 
-export default function Header({ title } : HeaderProps) {
+export default function Header({ title, showCancel = true } : HeaderProps) {
+    const navigation = useNavigation();
+
+    function handleGoBackToAppHomepage(){
+        navigation.navigate('OrphanagesMap')
+    }
     return (
         <View style={styles.container}>
-            <BorderlessButton>
+            <BorderlessButton onPress={navigation.goBack}>
                 <Feather name="arrow-left" size={24} color="#15b6d6"/>
             </BorderlessButton>
             <Text style={styles.title}>{title}</Text>
+            
+            { showCancel ? (
+                <BorderlessButton onPress={handleGoBackToAppHomepage}>
+                <Feather name="x" size={24} color="#ff669d"/>
+                </BorderlessButton>
+            ) : (
+                <View />
+            ) }
         </View>
+        
     );
 }
 
